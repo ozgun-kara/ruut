2ccbc691c4c159026015a4c0e7fd46ce2633bfc5
ozgun-kara <ozgunkara.devp@gmail.com>
2022-06-27 12:20:15 +0300

    Update finance_service.dart

--- lib/services/finance_service.dart
+++ lib/services/finance_service.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-import 'package:flutter/services.dart' as rootBundle;
-import 'package:ruut/models/credit_card_model.dart';
-import 'package:ruut/models/promotion_code_model.dart';
+
 
 class FinanceService {
   Future<List<CreditCardModel>> getCreditCardList() async {
