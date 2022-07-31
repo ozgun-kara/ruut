40940d54a67d88bd30e0e8f5cf6a0d49fcd3eedf
ozgun-kara <ozgunkara.devp@gmail.com>
2022-07-31 21:44:54 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,12 @@ import 'package:ruut/pages/finance_page.dart';
 import 'package:provider/provider.dart';
 import 'package:ruut/providers/finance_page_provider.dart';
 
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ruut',
-      home: FinancePage(),
-    );
-  }
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<FinancePageProvider>(
+      create: (BuildContext context) => FinancePageProvider(),
+    ),
+  ], child: const MyApp()));
 }
+
+
