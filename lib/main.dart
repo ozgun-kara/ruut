79d78a40e6f0150e56fa65c4e0ba249f598e8566
ozgun-kara <ozgunkara.devp@gmail.com>
2022-07-31 21:44:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:ruut/pages/finance_page.dart';
-import 'package:provider/provider.dart';
-import 'package:ruut/providers/finance_page_provider.dart';
+
 
 void main() {
   runApp(MultiProvider(providers: [
