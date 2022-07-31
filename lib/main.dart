fc172913e50ae0b7110bbbe87144a8086aca9e52
ozgun-kara <ozgunkara.devp@gmail.com>
2022-07-31 21:44:31 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:ruut/pages/finance_page.dart';
+import 'package:provider/provider.dart';
+import 'package:ruut/providers/finance_page_provider.dart';
 
 
-void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider<FinancePageProvider>(
-      create: (BuildContext context) => FinancePageProvider(),
-    ),
-  ], child: const MyApp()));
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
