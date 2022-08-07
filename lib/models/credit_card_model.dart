343590c5901b37eb85225327b21699ac46eb4d63
ozgun-kara <ozgunkara.devp@gmail.com>
2022-08-07 14:55:42 +0300

    Update credit_card_model.dart

--- lib/models/credit_card_model.dart
+++ lib/models/credit_card_model.dart
@@ -5,14 +5,16 @@ class CreditCardModel {
   String? expDate;
   String? icon;
 
+  CreditCardModel(this.name, this.cardNumber, this.expDate, this.icon);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
-    data['cardNumber'] = cardNumber;
-    data['expDate'] = expDate;
-    data['icon'] = icon;
-    return data;
+  CreditCardModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    cardNumber = json['cardNumber'];
+    expDate = json['expDate'];
+    icon = json['icon'];
   }
+
+
+
 }
