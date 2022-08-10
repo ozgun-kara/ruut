502f19978b0bde999f59d7d1f3381e5609227eee
ozgun-kara <ozgunkara.devp@gmail.com>
2022-08-10 12:09:57 +0300

    Update finance_page_provider.dart

--- lib/providers/finance_page_provider.dart
+++ lib/providers/finance_page_provider.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
+class FinancePageProvider with ChangeNotifier {
+  int selectedCreditCard = 0;
+  int selectedPromotionCode = 0;
 
+  void updateCreditCardValue(int cardIndex) {
+    selectedCreditCard = cardIndex;
+    notifyListeners();
+  }
+
+
+  
+}
