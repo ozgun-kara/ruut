9420fb520ce54c4e81ba464bdfafffc54043f6b4
ozgun-kara <ozgunkara.devp@gmail.com>
2023-01-02 12:39:55 +0300

    Update finance_page_provider.dart

--- lib/providers/finance_page_provider.dart
+++ lib/providers/finance_page_provider.dart
@@ -10,9 +10,5 @@ class FinancePageProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void updatePromotionCodeValue(int codeIndex) {
-    selectedPromotionCode = codeIndex;
-    notifyListeners();
-  }
-  
+
 }
