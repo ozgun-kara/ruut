75b040a526135253871188cf82e279d34ec8787a
ozgun-kara <ozgunkara.devp@gmail.com>
2023-02-17 13:41:26 +0300

    Update finance_page.dart

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -105,7 +105,36 @@ class FinancePage extends StatelessWidget {
             Row(
               children: [
 
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
 
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                      child: Text(
+                        "Finance",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "Cairo-VariableFont_wght",
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF01023C)),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                      child: Text(
+                        "Lorem Ipsum Dolor Sit Amed",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: "Cairo-VariableFont_wght",
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF8C8CB1)),
+                      ),
+                    ),
+
+                  ],
+                ),
         
               ],
             ),
@@ -157,44 +186,7 @@ class FinancePage extends StatelessWidget {
                       ),
                     ),
 
-                    ButtonTheme(
-                      minWidth: 98,
-                      height: 30,
-                      child: MaterialButton(
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(24),
-                                topRight: Radius.circular(24),
-                                bottomLeft: Radius.circular(24),
-                                bottomRight: Radius.circular(24)),
-                          ),
-                          color: const Color(0xFF01023C),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Text(
-                                'MANAGE YOUR BALANCE',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                  fontFamily: "Cairo-VariableFont_wght",
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFFF6F6FB),
-                                ),
-                              ),
 
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(14, 0, 0, 0),
-                                child: SvgPicture.asset(
-                                  'assets/images/CaretDown.svg',
-                                  width: 12,
-                                  height: 12,
-                                ),
-                              ),
-
-                            ],
-                          ),
-                          onPressed: () async {}),
-                    ),
 
                   ],
                 ),
