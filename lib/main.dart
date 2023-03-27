1591c42df4e21a8fd244773236f0bf6229a72eee
ozgun-kara <ozgunkara.devp@gmail.com>
2023-03-27 16:38:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+
 import 'package:ruut/pages/finance_page.dart';
 import 'package:provider/provider.dart';
 import 'package:ruut/providers/finance_page_provider.dart';
