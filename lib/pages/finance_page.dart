300d95b271e353b3578ca351bf01ea2bc8ec9874
ozgun-kara <ozgunkara.devp@gmail.com>
2023-04-05 11:29:31 +0300

    Update finance_page.dart

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -79,7 +79,19 @@ class FinancePage extends StatelessWidget {
                         onPressed: () async {}),
                   ),
           
-
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                    child: Builder(
+                      builder: (context) => IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/images/Bell.svg',
+                          width: 18.47,
+                          height: 22.4,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
 
                 ],
               ),
@@ -97,17 +109,7 @@ class FinancePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
 
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Text(
-                        "Finance",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: "Cairo-VariableFont_wght",
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF01023C)),
-                      ),
-                    ),
+
 
                     Padding(
                       padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
