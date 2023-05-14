8149280932bc004b361f7a6363c72e013e78b4ca
ozgun-kara <ozgunkara.devp@gmail.com>
2023-05-14 15:27:42 +0300

    Update finance_page.dart

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -1,4 +1,11 @@
-
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:math' as math;
+import 'package:provider/provider.dart';
+import 'package:ruut/models/credit_card_model.dart';
+import 'package:ruut/models/promotion_code_model.dart';
+import 'package:ruut/providers/finance_page_provider.dart';
+import 'package:ruut/services/finance_service.dart';
 
 class FinancePage extends StatelessWidget {
   FinancePage({Key? key}) : super(key: key);
@@ -37,40 +44,6 @@ class FinancePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
 
-                  ButtonTheme(
-                    minWidth: 98,
-                    height: 30,
-                    child: MaterialButton(
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(24),
-                              topRight: Radius.circular(24),
-                              bottomLeft: Radius.circular(24),
-                              bottomRight: Radius.circular(24)),
-                        ),
-                        color: const Color(0xFF01023C),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            SvgPicture.asset(
-                              'assets/images/Gift.svg',
-                              width: 19.63,
-                              height: 19.63,
-                            ),
-                            const Text(
-                              'Get \$50',
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  fontFamily: "Cairo-VariableFont_wght",
-                                  fontWeight: FontWeight.w900,
-                                  color: Color(0xFF28CB69)),
-                            ),
-
-
-                          ],
-                        ),
-                        onPressed: () async {}),
-                  ),
           
                   Padding(
                     padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
