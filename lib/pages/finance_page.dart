265b5958e0aef8ecab6cfe42b250e8d8ba53162f
ozgun-kara <ozgunkara.devp@gmail.com>
2023-06-30 11:08:39 +0300

    Update finance_page.dart

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -247,45 +247,7 @@ class FinancePage extends StatelessWidget {
                   ),
                 ),
 
-                ButtonTheme(
-                  minWidth: 98,
-                  height: 30,
-                  child: MaterialButton(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(24),
-                            topRight: Radius.circular(24),
-                            bottomLeft: Radius.circular(24),
-                            bottomRight: Radius.circular(24)),
-                      ),
-                      color: const Color(0xFFF6F6FB),
-                      elevation: 0,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Text(
-                            'ADD NEW',
-                            style: TextStyle(
-                              fontSize: 10,
-                              letterSpacing: 1,
-                              fontFamily: "Cairo-VariableFont_wght",
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xFF4D4E74),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-                            child: SvgPicture.asset(
-                              'assets/images/Plus.svg',
-                              width: 8,
-                              height: 8,
-                            ),
-                          ),
-                        ],
-                      ),
 
-                      onPressed: () async {}),
-                ),
         
               ],
             ),
