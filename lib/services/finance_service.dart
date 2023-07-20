3e83f0614fac7bf6f887397715ce3ab4b2e060fe
ozgun-kara <ozgunkara.devp@gmail.com>
2023-07-20 11:03:44 +0300

    Update finance_service.dart

--- lib/services/finance_service.dart
+++ lib/services/finance_service.dart
@@ -13,6 +13,12 @@ class FinanceService {
     return list.map((e) => CreditCardModel.fromJson(e)).toList();
   }
 
+  Future<List<PromotionCodeModel>> getPromotionCodeList() async {
+    final jsondata = await rootBundle.rootBundle
+        .loadString('database/promotion_code_list.json');
+    final list = json.decode(jsondata) as List<dynamic>;
 
+    return list.map((e) => PromotionCodeModel.fromJson(e)).toList();
+  }
   
 }
