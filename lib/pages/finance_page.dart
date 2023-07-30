2e75c95518a598bef168719c25d912a8b36a62ed
ozgun-kara <ozgunkara.devp@gmail.com>
2023-07-30 11:46:00 +0300

    Update finance_page.dart

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -109,17 +109,7 @@ class FinancePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
 
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Text(
-                        "Finance",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: "Cairo-VariableFont_wght",
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF01023C)),
-                      ),
-                    ),
+
 
                     Padding(
                       padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
