c7d52d9c9a2b6ebcd3f128f74e7b121558213346
ozgun-kara <ozgunkara.devp@gmail.com>
2023-08-03 15:16:47 +0300

    Update finance_service.dart

--- lib/services/finance_service.dart
+++ lib/services/finance_service.dart
@@ -168,7 +168,42 @@ class _FinancePageState extends State<FinancePage> {
                       ),
                     ),
 
-
+                    ButtonTheme(
+                      minWidth: 98,
+                      height: 30,
+                      child: MaterialButton(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(24),
+                                topRight: Radius.circular(24),
+                                bottomLeft: Radius.circular(24),
+                                bottomRight: Radius.circular(24)),
+                          ),
+                          color: const Color(0xFF01023C),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Text(
+                                'MANAGE YOUR BALANCE',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontFamily: "Cairo-VariableFont_wght",
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFF6F6FB),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(14, 0, 0, 0),
+                                child: SvgPicture.asset(
+                                  'assets/images/CaretDown.svg',
+                                  width: 12,
+                                  height: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                          onPressed: () async {}),
+                    ),
 
 
                   ],
