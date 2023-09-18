daf0e2fe3ac76f555e1954a8b6fda46baa7c745f
ozgun-kara <ozgunkara.devp@gmail.com>
2023-09-18 13:33:07 +0300

    Update promotion_code_model.dart

--- lib/models/promotion_code_model.dart
+++ lib/models/promotion_code_model.dart
@@ -6,5 +6,26 @@ class PromotionCodeModel {
   String? usesLeft;
   String? worth;
 
+  PromotionCodeModel(
+      this.name, this.codeNumber, this.expDate, this.usesLeft, this.worth);
 
+  PromotionCodeModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    codeNumber = json['codeNumber'];
+    expDate = json['expDate'];
+    usesLeft = json['usesLeft'];
+    worth = json['worth'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['codeNumber'] = codeNumber;
+    data['expDate'] = expDate;
+    data['usesLeft'] = usesLeft;
+    data['worth'] = worth;
+    return data;
+  }
 }
