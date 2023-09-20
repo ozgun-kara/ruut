38dc0fffa794036b4a18fbc70b0d1d9ef08f3b0c
ozgun-kara <ozgunkara.devp@gmail.com>
2023-09-20 13:34:31 +0300

    Update finance_page.dart

--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -148,31 +148,7 @@ class FinancePage extends StatelessWidget {
                       ),
                     ),
 
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
-                      child: Text(
-                        "Your Balance: ",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontFamily: "Cairo-VariableFont_wght",
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFFF6F6FB),
-                        ),
-                      ),
-                    ),
 
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                      child: Text(
-                        "\$1,250",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontFamily: "Cairo-VariableFont_wght",
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFF6F6FB),
-                        ),
-                      ),
-                    ),
 
                     ButtonTheme(
                       minWidth: 98,
