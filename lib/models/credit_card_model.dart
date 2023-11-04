bc23774bff5de8f234919bb3d68a5b5150f23ae6
ozgun-kara <ozgunkara.devp@gmail.com>
2023-11-04 12:21:06 +0300

    Update credit_card_model.dart

--- lib/models/credit_card_model.dart
+++ lib/models/credit_card_model.dart
@@ -1,9 +1,5 @@
 class CreditCardModel {
-  int? id;
-  String? name;
-  String? cardNumber;
-  String? expDate;
-  String? icon;
+
 
   CreditCardModel(this.name, this.cardNumber, this.expDate, this.icon);
 
