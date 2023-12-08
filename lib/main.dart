bed397fd6a3d2d2b4d1451ed0f57cb7d7e09b78d
ozgun-kara <ozgunkara.devp@gmail.com>
2023-12-08 19:32:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,16 @@ void main() {
 }
 
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ruut',
+      home: FinancePage(),
+    );
+  }
+}
