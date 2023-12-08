93a076199ddb3c3120e75c58bc12364cbdba8d15
ozgun-kara <ozgunkara.devp@gmail.com>
2023-12-08 19:32:28 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-
+import 'package:flutter/material.dart';
+import 'package:ruut/pages/finance_page.dart';
+import 'package:provider/provider.dart';
+import 'package:ruut/providers/finance_page_provider.dart';
 
 void main() {
   runApp(MultiProvider(providers: [
@@ -9,16 +12,3 @@ void main() {
 }
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ruut',
-      home: FinancePage(),
-    );
-  }
-}
