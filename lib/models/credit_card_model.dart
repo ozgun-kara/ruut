1d43f77432f96d99d68695f5ec7b6ebf68645288
ozgun-kara <ozgunkara.devp@gmail.com>
2024-01-09 14:31:33 +0300

    Update credit_card_model.dart

--- lib/models/credit_card_model.dart
+++ lib/models/credit_card_model.dart
@@ -15,5 +15,13 @@ class CreditCardModel {
     icon = json['icon'];
   }
 
-
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['cardNumber'] = cardNumber;
+    data['expDate'] = expDate;
+    data['icon'] = icon;
+    return data;
+  }
 }
