9b0008480eaba739f530813a00ef500793d81872
ozgun-kara <ozgunkara.devp@gmail.com>
2024-01-13 13:16:21 +0300

    Update finance_service.dart

--- lib/services/finance_service.dart
+++ lib/services/finance_service.dart
@@ -4,13 +4,7 @@ import 'package:ruut/models/credit_card_model.dart';
 import 'package:ruut/models/promotion_code_model.dart';
 
 class FinanceService {
-  Future<List<CreditCardModel>> getCreditCardList() async {
-    final jsondata = await rootBundle.rootBundle
-        .loadString('database/credit_card_list.json');
-    final list = json.decode(jsondata) as List<dynamic>;
 
-    return list.map((e) => CreditCardModel.fromJson(e)).toList();
-  }
 
   Future<List<PromotionCodeModel>> getPromotionCodeList() async {
     final jsondata = await rootBundle.rootBundle
