58ce8f45380d6b28cd9b960d05e9cf8d19a073ca
ozgun-kara <ozgunkara.devp@gmail.com>
2024-04-03 15:12:13 +0300

    Update promotion_code_model.dart

--- lib/models/promotion_code_model.dart
+++ lib/models/promotion_code_model.dart
@@ -18,5 +18,14 @@ class PromotionCodeModel {
     worth = json['worth'];
   }
 
-
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['codeNumber'] = codeNumber;
+    data['expDate'] = expDate;
+    data['usesLeft'] = usesLeft;
+    data['worth'] = worth;
+    return data;
+  }
 }
